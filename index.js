282eb0d8899e90cbb23f6dfc84fa659af7016b19
annecie <ajbexcelsior@gmail.com>
2021-11-15 15:20:56 +0000

    code challenge completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //write your code here
+const findLongestWord = (words) =>{
+ let longestWord = "";
+//  let currentWord = "";   
+ for (let word of words) {
+     if (word.length > longestWord.length){
+    //  return currentWord;
+        longestWord = word
+    }
+    
+}
+return longestWord
+}
+console.log(findLongestWord(["jazzy", "jumpy", "quaky"]))
